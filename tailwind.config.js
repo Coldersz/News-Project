bfaf413583419a4bb1553d4eb839b720096856b9
Coldersz <achmad.dzulfian7@gmail.com>
2021-09-03 19:20:52 +0700

    Update Hero component (2)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "redder": "#630A10",
+      },
+    },
     fontFamily: {
-      head: ['Playfair Display', 'serif'],
-      body: ['Nunito', 'Sans-serif']
-    }
+      head: ["Playfair Display", "serif"],
+      body: ["Nunito", "Sans-serif"],
+    },
   },
   variants: {
     extend: {
-      translate: ['group-hover']
+      translate: ["group-hover"],
     },
   },
   plugins: [],
-}
+};
