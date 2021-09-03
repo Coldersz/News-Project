4dfcaac141608e6a2588a347a46ad4e21ab9e052
Coldersz <achmad.dzulfian7@gmail.com>
2021-09-03 21:32:08 +0700

    Add Browse page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,97 @@
 <template>
   <div class="hello">
-    <div class="min-h-screen bg-gradient-to-br from-black via-gray-800 to-black bg-cover w-full flex px-10">
+    <div
+      class="
+        min-h-screen
+        bg-gradient-to-br
+        from-black
+        via-gray-800
+        to-black
+        bg-cover
+        w-full
+        flex
+        px-10
+      "
+    >
       <div class="w-1/2 flex flex-wrap content-center">
-        <div class="font-head font-black text-4xl text-red-600 italic border-b-2 border-red-600 pb-1 mb-6">CATALOG NEWS</div>
-        <div class="font-head font-black text-white text-5xl laeding-normal mb-12 leading-tight">Katalog berita dari berbagai sumber</div>
+        <div
+          class="
+            font-head font-black
+            text-4xl text-red-600
+            italic
+            border-b-2 border-red-600
+            pb-1
+            mb-6
+          "
+        >
+          CATALOG NEWS
+        </div>
+        <div
+          class="
+            font-head font-black
+            text-white text-5xl
+            laeding-normal
+            mb-12
+            leading-tight
+          "
+        >
+          Katalog berita dari berbagai sumber
+        </div>
         <div class="flex gap-8">
-          <div class="font-body text-lg font-semibold bg-white px-10 py-4 rounded-full transform duration-300 hover:-translate-y-1 cursor-pointer tracking-wide align-middle">Lihat Headline</div>
-          <div class="font-body text-white text-lg font-semibold bg-transparent border-2 border-white px-16 py-4 rounded-full transform duration-300 hover:-translate-y-1 cursor-pointer tracking-wide align-middle">Jelajahi</div>
+          <div
+            class="
+              font-body
+              text-lg
+              font-semibold
+              bg-white
+              px-10
+              py-4
+              rounded-full
+              transform
+              duration-300
+              hover:-translate-y-1
+              cursor-pointer
+              tracking-wide
+              align-middle
+            "
+          >
+            Lihat Headline
+          </div>
+          <router-link
+            to="/browse"
+            class="
+              font-body
+              text-white text-lg
+              font-semibold
+              bg-transparent
+              border-2 border-white
+              px-16
+              py-4
+              rounded-full
+              transform
+              duration-300
+              hover:-translate-y-1
+              cursor-pointer
+              tracking-wide
+              align-middle
+            "
+            >Jelajahi</router-link
+          >
         </div>
       </div>
-      <div class="w-1/2 flex justify-end ">
-        <img src="@/assets/globe-3.png" alt="Photo by Markus Winkler on Unsplash" class="w-5/6 rounded-xl self-center transform duration-300 hover:scale-105">
+      <div class="w-1/2 flex justify-end">
+        <img
+          src="@/assets/globe-3.png"
+          alt="Photo by Markus Winkler on Unsplash"
+          class="
+            w-5/6
+            rounded-xl
+            self-center
+            transform
+            duration-300
+            hover:scale-105
+          "
+        />
       </div>
     </div>
   </div>
@@ -18,14 +99,13 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
---
+++ src/views/Browse.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div
+      class="
+        min-h-screen
+        bg-gradient-to-br
+        from-black
+        via-gray-800
+        to-black
+        bg-cover
+        w-full
+        px-10
+        py-14
+      "
+    >
+      <h1 class="text-white text-6xl font-body font-bold w-full block">
+        Jelajahi
+      </h1>
+      <div class="w-full my-10 h-auto grid gap-8 grid-cols-4">
+        <div
+          v-for="source in sources"
+          :key="source"
+          class="
+            bg-transparent
+            font-body font-semibold
+            text-center text-white text-xl
+            py-6
+            border-2 border-white
+            rounded-full
+            cursor-pointer
+            transform
+            duration-300
+            hover:bg-white
+            hover:text-gray-800
+          "
+        >
+          {{ source }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sources: [
+        "Liputan 6",
+        "Liputan 6",
+        "Liputan 6",
+        "Liputan 6",
+        "Liputan 6",
+        "Liputan 6",
+        "Liputan 6",
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
