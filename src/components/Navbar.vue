42177656974f427882e591ce2ea6dc912412587c
Coldersz <achmad.dzulfian7@gmail.com>
2021-09-08 14:55:32 +0700

    Add Jelajahi into nav item

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <div class="w-full bg-white sticky top-0 shadow px-10 py-4 flex justify-between place-content-center z-50">
     <router-link to="/" class="text-2xl text-red-600 font-bold font-head align-middle self-center cursor-pointer">CNEWS</router-link>
     <div class="flex flex-row justify-center gap-4">
+      <div class=" text-center p-2 font-body font-bold text-lg cursor-pointer border-b-2 border-transparent transform transition-all duration-300 hover:border-gray-800">Jelajahi</div>
       <div class=" text-center p-2 font-body font-bold text-lg cursor-pointer border-b-2 border-transparent transform transition-all duration-300 hover:border-gray-800">Olahraga</div>
       <div class=" text-center p-2 font-body font-bold text-lg cursor-pointer border-b-2 border-transparent transform transition-all duration-300 hover:border-gray-800">Bisnis</div>
       <div class=" text-center p-2 font-body font-bold text-lg cursor-pointer border-b-2 border-transparent transform transition-all duration-300 hover:border-gray-800">Teknologi</div>
