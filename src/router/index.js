aa7f1d6e62466a5a9753cead3c8d83adc329ec5e
Coldersz <achmad.dzulfian7@gmail.com>
2021-09-11 21:28:56 +0700

    Add Category/Education page

--- src/router/index.js
+++ src/router/index.js
@@ -62,6 +62,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Category/Entertainment.vue')
+  },
+  {
+    path: '/category/education',
+    name: 'Education',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Category/Education.vue')
   }
 ]
 
