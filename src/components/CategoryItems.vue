21d47650ef315f97e51c1a4921ee0a41e8871b12
Coldersz <achmad.dzulfian7@gmail.com>
2021-09-11 20:22:14 +0700

    Separate CategoryItems component from Category/Sports page

---
+++ src/components/CategoryItems.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="w-full flex justify-between">
+    <h1 class="text-5xl font-head font-bold w-full block">{{ title }}</h1>
+    <div class="flex gap-4">
+      <div
+        class="
+          bg-gray-900
+          rounded-full
+          place-self-center
+          px-10
+          py-3
+          text-white text-xl
+          font-body font-semibold
+          cursor-pointer
+          transform
+          duration-300
+          hover:-translate-y-1
+          hover:shadow-lg
+        "
+      >
+        Terbaru
+      </div>
+      <div
+        class="
+          bg-transparent
+          border-2 border-gray-900
+          rounded-full
+          place-self-center
+          px-8
+          py-3
+          text-xl
+          font-body font-semibold
+          cursor-pointer
+          transform
+          duration-300
+          hover:-translate-y-1
+          hover:shadow-lg
+        "
+      >
+        Terpopuler
+      </div>
+    </div>
+  </div>
+  <div class="w-full my-10 grid grid-cols-3 gap-4">
+    <div
+      v-for="article in articles"
+      :key="article"
+      class="w-full h-64 flex flex-col"
+    >
+      <a
+        :href="article.url || '/'"
+        target="_blank"
+        class="
+          top-headline
+          h-full
+          w-full
+          grid
+          place-content-between
+          bg-cover bg-black
+          hover:mix-blend-multiply
+          transition-all
+          transform
+          duration-300
+          group
+        "
+        id="headline"
+      >
+        <div class="block mx-6 mt-3 font-body font-bold opacity-0 invisible">
+          VIVA.CO.ID
+        </div>
+        <div
+          class="
+            block
+            font-head font-bold
+            text-xl text-white
+            m-6
+            transform
+            duration-300
+            group-hover:translate-x-2
+          "
+        >
+          {{
+            article ? article.title : "Silakan refresh beberapa saat lagi..."
+          }}
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "articles"],
+};
+</script>
+
+<style>
+</style>
--- src/views/Category/Sports.vue
+++ src/views/Category/Sports.vue
@@ -2,69 +2,23 @@
   <div class="bg-base">
     <Navbar />
     <div class="min-h-screen w-full px-10 py-14">
-      <div class="w-full flex justify-between">
-        <h1 class="text-5xl font-head font-bold w-full block">Olahraga</h1>
-        <div class="flex gap-4">
-          <div
-            class="
-              bg-gray-900
-              rounded-full
-              place-self-center
-              px-10
-              py-3
-              text-white text-xl
-              font-body font-semibold
-              cursor-pointer
-              transform
-              duration-300
-              hover:-translate-y-1
-              hover:shadow-lg
-            "
-          >
-            Terbaru
-          </div>
-          <div
-            class="
-              bg-transparent
-              border-2 border-gray-900
-              rounded-full
-              place-self-center
-              px-8
-              py-3
-              text-xl
-              font-body font-semibold
-              cursor-pointer
-              transform
-              duration-300
-              hover:-translate-y-1
-              hover:shadow-lg
-            "
-          >
-            Terpopuler
-          </div>
-        </div>
-      </div>
-      <div class="w-full my-10 grid grid-cols-3 gap-4">
-        <div v-for="article in articles" :key="article" class="w-full h-64 flex flex-col">
-          <a :href="article.url || '/'" target="_blank" class="top-headline h-full w-full grid place-content-between bg-cover bg-black hover:mix-blend-multiply transition-all transform duration-300 group" id="headline">
-            <div class="block mx-6 mt-3 font-body font-bold opacity-0 invisible">VIVA.CO.ID</div>
-            <div class="block font-head font-bold text-xl text-white m-6 transform duration-300 group-hover:translate-x-2">{{ article ? article.title : 'Silakan refresh beberapa saat lagi...' }}</div>
-          </a>
-        </div>
-      </div>
+      <Contents :title="title" :articles="articles" />
     </div>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar-TW";
+import Contents from "@/components/CategoryItems"
 
 export default {
   components: {
     Navbar,
+    Contents
   },
   data() {
     return {
+      title: "Olahraga",
       articles: [],
     };
   },
@@ -94,7 +48,7 @@ export default {
       setTimeout(() => {
         const headline = document.querySelectorAll(".top-headline")
         for (let i = 0; i < headline.length; i++) {
-          headline[i].style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.527), rgba(0, 0, 0, 0.3)), url(${this.articles[i].urlToImage})`
+          headline[i].style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.527), rgba(0, 0, 0, 0.5)), url(${this.articles[i].urlToImage})`
         }
       }, 100);
     }
